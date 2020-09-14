e6aebfb259ab4239141e15ab58e1a30272d7d8ba
GabijusKielius <gabijuskielius@gmail.com>
2020-09-14 18:14:03 +0300

    Initial setup

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { 
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true })
+    useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
